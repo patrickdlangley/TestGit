1f1a6dc8a23c52badd6c684dcefee76e0820de96
Patrick <Patrick@Patrick.Patrick>
2020-10-27 15:00:55 -0400

    First commit.

---
+++ Source.cpp
@@ -0,0 +1,19 @@
+/*
+TestGit
+Patrick Langley 
+10/27/2020
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	cout << "\nHello Git!";
+
+	cout << endl << endl;
+	system("pause");
+	return 0;
+}
